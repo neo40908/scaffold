50508b6ad6dff18b530636afe67f6dc2c7818f85
Aldo B <neo40908@gmail.com>
2023-05-07 01:38:01 +0000

    adding  second version to build

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return f"hi{x}"
 
-#var=
-result = add(2, 2)
-print(f"esta es la suma: 1,2 {result}")
+
+def add(x):
+    return x + 1
+
+
+def subtract(x):
+    return x - 1
+
+
+# var=
+# result = add(2, 2)
+# print(f"esta es la suma: 1,2 {result}")
--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,19 @@
-from hello import add
+from hello import toyou, add, subtract
 
-def test_add():
-    assert add(2,2)==4
+
+def setup_function(function):
+    print(f"Running Setup: {function.__name__}")
+    function.x = 10
+
+
+def teardown_function(function):
+    print(f"Running Teardown: {function.__name__}")
+    del function.x
+
+
+# def test_add():
+#    assert add(2,2)==4
+
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
